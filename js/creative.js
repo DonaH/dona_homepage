3be2e8803e0d419488e5fa84faae86d9cc5bb9d8
dona <dona.hsieh@gmail.com>
2017-03-06 13:14:34 -0800

    changed frontpage jpeg & updated copyright year

--- js/creative.js
+++ js/creative.js
@@ -28,7 +28,7 @@
     });
 
     // Randomize background-image
-    var images = ['catus1.jpg', 'griffith_observatory.jpg', 'workdesk.jpg'];
+    var images = ['telescopela.jpg', 'griffith_observatory.jpg', 'workdesk.jpg'];
     $('header').css({'background-image': 'url(img/' + images[Math.floor(Math.random() * images.length)] + ')'
     });
 
